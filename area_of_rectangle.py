eab0e9d46828931d10d9b9e05bad5904cbebd43f
JoanZ0019 <qzz0019@auburn.edu>
2022-04-12 20:29:35 -0500

    string to number function

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -4,6 +4,27 @@
 
 import sys
 
+def string_to_float_or_int(string):
+    """
+    Returns the string as a number
+    
+    Parameters
+    ----------
+    number : int or float
+        Number we want to convert to int or float based on presence of '.'
+
+    Returns
+    -------
+    int or float
+        The converted value in correct data type
+    """
+    if '.' in string:
+        number = float(string)
+    else:
+        number = int(string)
+
+    return number
+        
 
 def area_of_rectangle(height, width = None):
     """
@@ -30,26 +51,33 @@ def area_of_rectangle(height, width = None):
     14
     """
 
-    if width:
-        width = height
-    import pdb; pdb.set_trace()
+    #if width:
+    #    width = height
+#    import pdb; pdb.set_trace()
     area = height * width
     return area
+
 
 if __name__ == '__main__':
     if (len(sys.argv) < 2) or (len(sys.argv) > 3):
         message = (
-                "{script_name}: Expecting one or two command-line arguments:\n"
-                "\tthe height of a square or the height and width of a "
-                "rectangle".format(script_name = sys.argv[0]))
+                  "{script_name}: Expecting one or two command-line arguments:\n"
+                  "\tthe height of a square or the height and width of a "
+                  "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
-    height = sys.argv[1]
-    width = height
-    if len(sys.argv) > 3:
-        width = sys.argv[1]
 
-    area = area_of_rectangle(height, width)
-
+    elif len(sys.argv) == 2:
+        height = sys.argv[1]
+        height = string_to_float_or_int(height)   
+        width = height
+        area = area_of_rectangle(height, width)
+    else:
+        height= sys.argv[1]
+        height = string_to_float_or_int(height)
+        width = sys.argv[2]
+        width = string_to_float_or_int(width)
+        area = area_of_rectangle(height, width)
+ 
     message = "The area of a {h} X {w} rectangle is {a}".format(
             h = height,
             w = width,
